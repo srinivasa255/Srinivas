8e97c4cdbed15056e85dcf37ed1dddb3338b6c2b
chari <charituesday6@gmail.com>
2017-01-30 16:14:24 +0530

    first commit

---
+++ GitgubGitEclipse/src/com/pro/zurich/BaseClass.java
@@ -0,0 +1,19 @@
+package com.pro.zurich;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass {
+	
+	@BeforeMethod
+	  public void beforeMethod() {
+		 System.out.println("before");
+	  }
+
+	  @AfterMethod
+	  public void afterMethod() {
+		  System.out.println("after");
+	  }
+
+
+}
---
+++ GitgubGitEclipse/src/com/pro/zurich/NewTest.java
@@ -0,0 +1,13 @@
+package com.pro.zurich;
+
+import org.testng.annotations.Test;
+public class NewTest extends BaseClass{
+	
+	
+  @Test
+  public void f() {
+	  
+	  System.out.println("first");
+  }
+  
+}
