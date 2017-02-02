105a6e6ce882d678435da8a193bc99f87739456d
chari <charituesday6@gmail.com>
2017-02-02 12:13:36 +0530

    chari

--- GitgubGitEclipse/src/com/pro/zurich/BaseClass.java
+++ GitgubGitEclipse/src/com/pro/zurich/BaseClass.java
@@ -4,16 +4,14 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseClass {
-	
+
 	@BeforeMethod
-	  public void beforeMethod() {
-		 System.out.println("before");
-	  }
+	public void beforeMethod() {
+		System.out.println("before");
+	}
 
-	  @AfterMethod
-	  public void afterMethod() {
-		  System.out.println("after");
-	  }
-
-
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("after");
+	}
 }
